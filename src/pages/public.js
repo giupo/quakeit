492bfebb9b5a234fcd38c3c059711847235fdf46
Giuseppe Acito <giuseppe.acito@gmail.com>
2016-08-25 15:07:05 +0200

    Adds highcharts

--- src/pages/public.js
+++ src/pages/public.js
@@ -2,6 +2,17 @@
 
 import app from 'ampersand-app';
 import React from 'react';
+import ReactHighcharts from 'react-highcharts';
+
+var config = {
+  xAxis: {
+    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  },
+  series: [{
+    data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4]
+  }]
+};
+
 
 export default React.createClass({
   displayName : 'PublicPage',
@@ -20,28 +31,8 @@ export default React.createClass({
   },
 
   render () {
-    console.log('PublicPage called');
-    return  (
-        /* jshint ignore:start */
-        <div className='container'>
-          <header role='banner'>
-            <h1>Labelr</h1>
-          </header>
-          <div>
-           <p>We label stuff for you, beacuse, we can &trade;</p>
-              <a href='/login' onClick={this.onLoginClick} className='button button-large'>
-                <span className='mega octicon octicon-mark-github'></span> Login with Github
-              </a>
-          </div>
-
-          <div>
-           <p>We label stuff for you, beacuse, we can &trade;</p>
-              <a href='/otherPage' onClick={this.onOtherPageClick} className='button button-large'>
-                <span className='mega octicon octicon-mark-github'></span> Login with Github
-              </a>
-          </div>
-        </div>
-          /* jshint ignore:end */
+    return (
+      <ReactHighcharts config={config}></ReactHighcharts>
     );
   }
 });
